3af260e8aacae845df9f82ad1195d99e784ab782
Camron Naderi <camronnaderi57@gmail.com>
2023-10-19 10:41:28 -0700

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+const {connectDB, pool} = require("./db/connect")
+
+// routes 
+const userRouter = require("./routes/users")
+
+app.use(express.json())
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
+app.use("/api/v1/users", userRouter)  
+
+const startApp = async () => {
+  try {
+    await connectDB()
+    app.listen(process.env.PORT, console.log(`App started on PORT ${process.env.PORT}`))
+  } catch (err) {
+    console.log("an error occured", err)
+  }
+}
+
+startApp()
