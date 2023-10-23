8ffd53da43f4927952a3df7d2f56b49dcef08a7a
Jesse Sandhu <s.jesse23@gmail.com>
2023-10-23 16:19:29 -0700

    adding search bar to grab search query / searchHanlder function and send to button

---
+++ frontend/src/components/search/SearchBar.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import SearchButton from './SearchButton';
+import SearchContainer from './SearchContainer';
+function SearchBar(props) {
+  const [search, setSearch] = useState('');
+
+  const inputChangeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchHandler = () => {
+    //can use search query here for futher processing with sending data to server
+    //props.onClick();
+    console.log('search query:', search);
+    // useEffect(() => {
+    fetch('http://localhost:3000/api/search')
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the JSON data from the response here
+        console.log(data);
+      });
+    // }, []);
+    // searchHandler should be a hook and within use useEffect eg useSearchHandler
+  };
+
+  return (
+    <div className='searchBar'>
+      <SearchButton label={props.label} clickHandler={searchHandler} />
+      <div className='searchIconBar'>
+        <input
+          className='inputSize'
+          type="text"
+          placeholder="Type your search here and hit spin"
+          value={search}
+          onChange={inputChangeHandler}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
