208c15d5c5718c7a22b9885eec397b74d8054dc7
Jesse Sandhu <s.jesse23@gmail.com>
2023-10-25 16:04:07 -0700

    adding mock data and modifying likedrecipe component

--- frontend/src/components/LikedRecipe.jsx
+++ frontend/src/components/LikedRecipe.jsx
@@ -1,39 +1,80 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import RecipeItem from "./RecipeItem";
-import { Card } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 
-const RecipeItemGrid = ({ handleCardClick, activeModal }) => {
+const mockData = [
+  {
+    id: 1,
+    imgSrc: "mushroomPasta.png",
+    name: "Mushroom Pasta",
+    description: "Delicious mushroom pasta with creamy sauce.",
+  }
+  // Add more recipe objects as needed
+];
+
+const LikedRecipe = ({ handleCardClick, activeModal }) => {
   return (
-    <Card sx={{ width: "50vw", padding: 3, margin: 3, borderRadius: 9 }}>
-      <Grid container spacing={3} justifyContent="center">
-        <Grid item xs={4}>
-          <RecipeItem
-            photo="mushroomPasta.png"
-            RecipeName={"Mushroom Pasta"}
-            handleCardClick={handleCardClick}
-            activeModal={activeModal}
-          />
+    <Card sx={{ width: "auto", padding: 3, margin: "0 auto", borderRadius: 9 }}>
+      <Grid container spacing={3} justifyContent="center" >
+        <Grid item xs={3}>
+          <Card sx={{ padding: 2, borderRadius: 9, boxShadow: 3, margin: "0 auto", width: "80%", height: "150px" }}>
+            <CardContent>
+              <img src={mockData[0].imgSrc} alt={mockData[0].name} style={{ maxWidth: "150px", maxHeight: "150px", display: "block" }} />
+
+              <Typography variant="h6" component="div" sx={{ textAlign: "center" }}>
+                {mockData[0].name}
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item xs={4}>
-          <RecipeItem
-            photo="BuddhaBowl.png"
-            RecipeName={"Buddha Bowl"}
-            handleCardClick={handleCardClick}
-            activeModal={activeModal}
-          />
+        <Grid item xs={6}>
+          <Card sx={{ padding: 2, borderRadius: 9, boxShadow: 3, margin: "0 auto", width: "80%", height: "150px" }}>
+            <CardContent>
+              <Typography variant="h6" component="div" sx={{ textAlign: "center" }}>
+                Description
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ textAlign: "center" }}>
+                {mockData[0].description}
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item xs={4}>
-          <RecipeItem
-            photo="ButterChicken.png"
-            RecipeName={"Butter Chicken"}
-            handleCardClick={handleCardClick}
-            activeModal={activeModal}
-          />
+        <Grid item xs={3}>
+          <Card sx={{
+            padding: 2,
+            borderRadius: 9,
+            boxShadow: 3,
+            margin: "0 auto",
+            width: "100%",
+            height: "150px"
+          }}>
+            <CardContent >
+              <Typography variant="h6" component="div" sx={{ textAlign: "center" }}>
+                Options
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ textAlign: "center" }}>
+                Content goes here.
+              </Typography>
+              <Grid container spacing={2} justifyContent="center"> {/* Nest another Grid container */}
+                <Grid item xs={4}>
+                  <div style={{ backgroundColor: "lightblue", height: "50px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <span style={{ fontSize: "12px" }}>Delete</span>
+                  </div>
+                </Grid>
+                <Grid item xs={4}>
+                  <div style={{ backgroundColor: "lightcoral", height: "50px", justifyContent: "center" }}>Rate</div>
+                </Grid>
+                <Grid item xs={4}>
+                  <div style={{ backgroundColor: "lightgreen", height: "50px", justifyContent: "center" }}>Share</div>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </Card>
   );
 };
 
-export default RecipeItemGrid;
+export default LikedRecipe;
