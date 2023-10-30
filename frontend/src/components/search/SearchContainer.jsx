d019491b56a98ae2ffa0879972fa34025c0ff6a5
Tagellajore <tagelsft9@gmail.com>
2023-10-30 12:48:55 -0400

    trying to merge

--- frontend/src/components/search/SearchContainer.jsx
+++ frontend/src/components/search/SearchContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SearchButton from './SearchButton';
 import SearchBar from './SearchBar';
-import '/Users/camronnaderi/lighthouse/RecipeRoulette/frontend/src/Search.css';
+import '/home/labber/RecipeRoulette/frontend/src/Search.css';
 
 function SearchContainer(props) {
   const [imgSpin, setImgSpin] = useState(false);
