2bca86a078947e46abb17099444f5be51314192f
Camron Naderi <camronnaderi57@gmail.com>
2023-10-29 20:17:16 -0700

    finish button card and description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,12 @@
-import React, { useState } from 'react';
-import RecipeItemGrid from './components/RecipeItemGrid';
-import RecipeModal from './components/RecipeModal';
-import SearchContainer from './components/search/SearchContainer';
-import NavigationBar from './components/NavigationBar';
+import React from 'react';
+import HomeRoute from './routes/HomeRoute';
 
 
 function App() {
 
-  const [activeModal, setActiveModal] = useState(null);
-  const [recipeData, setRecipeData] = useState([]);
-
-  const handleCardClick = (RecipeName, photo,) => {
-    if (activeModal === null) {
-      setActiveModal(RecipeName);
-    }
-  };
-
-  const handleCloseModal = () => {
-    setActiveModal(null);
-  };
-
   return (
     <div className="App">  
-      <NavigationBar /> 
-      <SearchContainer setRecipeData={setRecipeData} />
-      <RecipeItemGrid handleCardClick={handleCardClick} activeModal={activeModal} recipeData={recipeData}/>
-      {activeModal && <RecipeModal RecipeName={activeModal} onClose={handleCloseModal} recipeData={recipeData}/>}
+     <HomeRoute />
     </div>
   );
 }
--- frontend/src/components/search/SearchButton.jsx
+++ frontend/src/components/search/SearchButton.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react';
-import SearchBar from './SearchBar';
+import React from 'react';
+
 
 function SearchButton(props) {
 
 
   return (
     <div className="btn-card">
+      <h2>Welcome to Recipe Roulette!</h2>
+<ul>
+  <li>Click the button below to start the roulette.</li>
+  <li>We'll pick a random recipe for you.</li>
+  <li>If you like it, awesome! If not, just spin again.</li>
+</ul>
+<p className="btn-card-text">Go ahead, take a spin and spice up your meal plans!</p>
       <button className='bttn' onClick={props.clickHandler} >
         {props.label}
       </button>
