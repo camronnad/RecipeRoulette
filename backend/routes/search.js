3df287ea375ec43dbed602dfc3fc22e7fe61f876
Camron Naderi <camronnaderi57@gmail.com>
2023-11-13 09:49:00 -0800

    remove search comments

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -12,14 +12,14 @@ const searchRouter = (pool) => {
       const apiKey = process.env.APIKEY;
       const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${apiKey}&number=3`;
 
-      // Fetch the recipe data
+      
       const response = await axios.get(url);
       const data = response.data;
       const idsString = data.results.map(rec => rec.id).join(",");
       const bulkUrl = `https://api.spoonacular.com/recipes/informationBulk?ids=${idsString}&apiKey=${apiKey}`;
       const bulkResponse = await axios.get(bulkUrl);
       const bulkData = bulkResponse.data;
-      // Send recipe data back to the front-end
+      
       res.json(bulkData);
     } catch (error) {
       console.error('Error fetching data:', error);
